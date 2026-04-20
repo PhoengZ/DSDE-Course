31c7e9412fdf5669b18fe034cb5babfc519af061
Phaolap Kulteera <pholapcondo11@gmail.com>
2026-04-20 20:30:56 +0700

    Practice befre exam

---
+++ Pandas/01_pandas_02_2025s2_2/student.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import json
+
+"""
+    ASSIGNMENT 1 (STUDENT VERSION):
+    Using pandas to explore youtube trending data from (videos.csv and category_id.json) and answer the questions.
+"""
+
+def Q1():
+    """
+        1. How many rows are there in the videos.csv after removing duplications?
+        - To access 'videos.csv', use the path '/data/videos.csv'.
+    """
+    df = pd.read_csv("videos.csv").drop_duplicates()
+    return df.shape[0]
+
+def Q2(vdo_df):
+    '''
+        2. How many VDO that have "dislikes" more than "likes"? Make sure that you count only unique title!
+            - videos.csv has been loaded into memory and is ready to be utilized as vdo_df
+            - The duplicate rows of vdo_df have been removed.
+    '''
+    # vdo_df = vdo_df.drop_duplicates(subset=['title'], keep='first')
+    return vdo_df.loc[vdo_df['dislikes'] > vdo_df['likes'], 'title'].nunique()
+
+def Q3(vdo_df):
+    '''
+        3. How many VDO that are trending on 22 Jan 2018 with comments more than 10,000 comments?
+            - videos.csv has been loaded into memory and is ready to be utilized as vdo_df
+            - The duplicate rows of vdo_df have been removed.
+            - The trending date of vdo_df is represented as 'YY.DD.MM'. For example, January 22, 2018, is represented as '18.22.01'.
+    '''
+    # TODO: Paste your code here
+    return None
+
+def Q4(vdo_df):
+    '''
+        4. Which trending date that has the minimum average number of comments per VDO?
+            - videos.csv has been loaded into memory and is ready to be utilized as vdo_df
+            - The duplicate rows of vdo_df have been removed.
+    '''
+    # TODO:  Paste your code here
+    return None
+
+def Q5(vdo_df):
+    '''
+        5. Compare "Sports" and "Comedy", how many days that there are more total daily views of VDO in "Sports" category than in "Comedy" category?
+            - videos.csv has been loaded into memory and is ready to be utilized as vdo_df
+            - The duplicate rows of vdo_df have been removed.
+            - You must load the additional data from 'category_id.json' into memory before executing any operations.
+            - To access 'category_id.json', use the path '/data/category_id.json'.
+    '''
+    # TODO:  Paste your code here
+    return None
